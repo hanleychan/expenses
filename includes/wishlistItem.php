676d8cc94fe9d88d68f0c9b367a12c084a6127a0
Hanley Chan <hanleychan@gmail.com>
2016-02-19 23:45:51 -0800

    Resolved problem deleting wishlist groups

--- includes/wishlistItem.php
+++ includes/wishlistItem.php
@@ -21,6 +21,35 @@ class WishlistItem extends Currency {
 
         return true;
     }
+
+
+    /**
+     * Returns whether there exists any items belonging to a group for a user
+     */
+    public static function hasItemWithGroup($userID, $groupID) {
+        $sql = 'SELECT * FROM wishlistitems WHERE userID = ? AND groupID = ?';
+        $paramArray = array($userID, $groupID);
+        $result = self::findBySql($sql, $paramArray);
+
+        if($result) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+
+    /**
+     * Returns the number of wishlist items a user has
+     */
+    public static function getNumItems($userID) {
+        $sql = 'SELECT * FROM wishlistitems WHERE userID = ?';
+        $paramArray = array($userID);
+        $result = self::findBySql($sql, $paramArray);
+
+        return count($result);
+    }
 }
 
 ?>
